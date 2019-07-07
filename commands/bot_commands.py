836ef78f3012ac555ca0382cae068c1f44a73096
Bruno L. Carli <brunolcarli@gmail.com>
2019-07-07 20:32:23 -0300

    ADD quote commands

--- commands/bot_commands.py
+++ commands/bot_commands.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 from decouple import config
+import requests
+import json
+from random import choice
+from commands.queries import get_quote_mutation
+from settings import LISA_URL
 
 
 
@@ -21,3 +26,49 @@ async def repo(bot, repo_name=''):
     '''
     repo_url = config('GITLAB_REPO') + '/{}'.format(repo_name)
     await bot.send(repo_url)
+
+@client.command()
+async def quote(bot, *phrase):
+    '''
+    Saves a quoted message to be forever remembered.
+    '''
+    if phrase:
+        message = ' '.join(word for word in phrase)
+        payload = get_quote_mutation(message)
+
+        headers = {
+            'content-type': "application/json"
+        }
+        
+        response = requests.request("POST", LISA_URL, data=payload, headers=headers)
+        response = json.loads(response.text)
+        response = response['data']['createR2Quote'].get('response')
+
+    else:
+        response = 'Insira alguma mensagem!'
+        response += '\n`r2/quote foo baz`'
+
+    await bot.send(response)
+
+@client.command()
+async def random_quote(bot):
+    '''
+    Returns a random quote
+    '''
+    payload = "{\"query\":\"query{\\n  r2Quotes\\n}\"}"
+    headers = {
+        'content-type': "application/json"
+    }
+
+    response = requests.request("POST", LISA_URL, data=payload, headers=headers)
+    response = json.loads(response.text)
+    quotes = response['data'].get('r2Quotes')
+
+    await bot.send(choice(quotes))
+
+@client.command()
+async def remember(bot):
+    '''
+    Remembers you something important
+    '''
+    await bot.send('`LEIAM A DOCUMENTAÇÃO!`')
