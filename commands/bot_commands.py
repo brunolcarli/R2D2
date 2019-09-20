8f205f4ef62417f3c66fbe677f40a8fe3ecad2d1
Bruno Luvizotto Carli <bruno.carli@smartgreen.net>
2019-09-20 18:01:15 -0300

    ADD handler para responder mensagens no chat

--- commands/bot_commands.py
+++ commands/bot_commands.py
@@ -13,10 +13,74 @@ from discord.ext import commands
 from decouple import config
 import requests
 from commands.queries import get_quote_mutation
-from settings import LISA_URL, GENERAL_CHANNEL
+from settings import LISA_URL, GENERAL_CHANNEL, R2ID
+from utils.nlp import (get_offense_level, get_the_right_answer,
+                       binary_wordmatch, text_classifier,
+                       basic_preprocess)
+
+from utils.output_vectors import (opinions, insufficiency_recognition,
+                                  offended)
+
 
 
 client = commands.Bot(command_prefix='r2/')
+
+
+@client.event
+async def on_message(message):
+    """
+    Handler for chat event messages.
+    """
+
+    # Processa somente mensagens que não são do próprio R2D2
+    if message.author.id == int(R2ID):
+        return
+
+    bot_mention = f'<@{R2ID}>'
+    channel = message.channel
+
+    # Não processa mensagens designadas à outros membros
+    if '@' in message.content and bot_mention not in message.content:
+        return
+
+    greeting_msgs = [
+        'oi', 'olá', 'saudações', 'namastê', 'bom dia', 'boa tarde'
+    ]
+
+    # Mensagens de despedida
+    byebye_msgs = [
+        'tchau', 'até logo', 'até mais', 'até breve', 'adeus', 'bye', 'ciao'
+    ]
+    try:
+        chat_message = basic_preprocess(message.content)
+    except Exception as ex:
+        # TODO fazer um log melhor disso
+        print(ex)
+    else:
+        # Se a mensagem enviada for um cumprimento
+        if binary_wordmatch(chat_message, greeting_msgs):
+            await channel.send(
+                f'{choice(greeting_msgs).capitalize()} <@{message.author.id}>!'
+            )
+
+        # se for uma mensagem de despedida
+        elif binary_wordmatch(chat_message, byebye_msgs):
+            await channel.send(
+                f'{choice(byebye_msgs).capitalize()} <@{message.author.id}>!'
+            )
+
+        try:
+            is_offensive, offensivness = get_offense_level(
+                basic_preprocess(message.content.lower())
+            )
+        except Exception as ex:
+            # TODO escrever um log adequado
+            print(ex)
+
+        else:
+            # se for ofensivo
+            if is_offensive:
+                await channel.send(choice(offended))
 
 
 @client.event
@@ -27,6 +91,7 @@ async def on_member_join(member):
     # TODO melhorar essa mensagemd e boas vindas com um embedding bem bonito
     channel = client.get_channel(GENERAL_CHANNEL)
     await channel.send('\n\n```BEM VINDO```\n\n```WELCOME```\n\n')
+    return
 
 
 @client.event
