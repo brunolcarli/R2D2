95c73ed31bfb73ce161484d137e200428c7f814a
Bruno Luvizotto Carli <bruno.carli@smartgreen.net>
2019-09-20 18:00:00 -0300

    ADD download dos pacotos de nlp na inicialização

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 from settings import TOKEN
 from commands.bot_commands import client
-
+import nltk
 
 if __name__ == "__main__":
+    nltk.download('punkt')
+    nltk.download('stopwords')
     client.run(TOKEN)
