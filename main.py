389775de2e919b88f98754027d2e55f02c57c96b
Bruno L. Carli <brunolcarli@gmail.com>
2023-04-01 16:06:28 -0300

    ADD keep alive with flask

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 from settings import TOKEN
 from commands.bot_commands import client
+from commands.keep_alive import keep_aive
 
 
 if __name__ == "__main__":
+    keep_alive()
     client.run(TOKEN)
